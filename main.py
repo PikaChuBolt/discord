146046d8f3770f9d6815d9030393ac735b9a3219
PikaChuBolt <kingtaym@gmail.com>
2025-07-27 22:19:28 -0500

    test

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.members = True  # Required to manage roles
+intents.message_content = True # Required to read message content
+intents.reactions = True # Required to detect reactions
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
 
 @client.event
 async def on_ready():
